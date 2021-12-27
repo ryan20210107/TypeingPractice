640e3534d62c7765c8d6c19e023604534bb52ab1
ryan20210107 <77113917+ryan20210107@users.noreply.github.com>
2021-12-27 10:35:53 +0800

    Add files via upload
    Typing practice tool v1.0.0

---
+++ Typing/Typing/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Typing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ckb_sound = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 224);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(464, 214);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(21, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Consolas", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 97);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ckb_sound
+            // 
+            this.ckb_sound.AutoSize = true;
+            this.ckb_sound.Location = new System.Drawing.Point(25, 171);
+            this.ckb_sound.Name = "ckb_sound";
+            this.ckb_sound.Size = new System.Drawing.Size(54, 16);
+            this.ckb_sound.TabIndex = 4;
+            this.ckb_sound.Text = "Sound";
+            this.ckb_sound.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Consolas", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 98);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NG";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 443);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ckb_sound);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Typing Exercise v1.0.0 (2021.12.24)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ckb_sound;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Typing/Typing/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Typing
+{
+    public partial class Form1 : Form
+    {
+        //key value reference 
+        // http://web.tnu.edu.tw/me/study/moodle/tutor/vb6/tutor/r03/index.htm
+
+        //play wav https://blog.kkbruce.net/2019/03/csharpformusicplay.html#.YcWFE2hBxGM
+        //wav download https://www.youtube.com/watch?v=l59FoIJXeOg 
+        //wav download https://drive.google.com/drive/folders/1MC1vxAaPhOdjULhccmDZaM6nlemkTzzm?fbclid=IwAR0LKtuH0hHcBQ2hFM5RnkyD-yhf_E-6bRuY4tIIT3OuBK5HlZlMDVa_rdk
+        //wav download 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Visible = false;
+
+        }
+
+        int User_input_key = 0;
+        bool inputFlag = false;
+        bool loopFlag1 = false;
+        bool loopFlag2 = false;
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            User_input_key = e.KeyValue;
+            inputFlag = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            loopFlag1 = true;
+            var player = new SoundPlayer();
+
+            while (loopFlag1)
+            {
+                Thread.Sleep(50);
+                Application.DoEvents();
+
+                string StrTemp = "";
+                int Question_key = rnd.Next(65,90); //A~Z = 65 ~90
+                label1.Text = ((char)Question_key).ToString();
+                Application.DoEvents();
+
+                //compare Question_key and User_input_key
+                loopFlag2 = true;
+                while (loopFlag2)
+                {
+                    Thread.Sleep(50);
+                    Application.DoEvents();
+
+                    if (inputFlag == true)
+                    {
+                        inputFlag = false;
+                        //StrTemp = User_input_key.ToString() + string.Format(" ,{0:D}", GetKey);
+                        StrTemp = ((char)User_input_key).ToString() + string.Format(" ,{0:D}", User_input_key);
+                        richTextBox1.AppendText("\n" + StrTemp);
+
+                        
+                        if (Question_key == User_input_key)
+                        {
+                            loopFlag2 = false;
+                            label2.Visible = false;
+                            Application.DoEvents();
+
+                            if (ckb_sound.Checked == true)
+                            {
+                                player.SoundLocation = "sound_ok.wav";
+                                player.LoadAsync();
+                                player.PlaySync();
+                            }
+
+                            Application.DoEvents();
+                        }
+                        else
+                        {
+                            label2.ForeColor = Color.Red;
+                            label2.Visible = true;
+                            Application.DoEvents();
+
+                            if (ckb_sound.Checked == true)
+                            {
+                                player.SoundLocation = "sound_ng.wav";
+                                player.LoadAsync();
+                                player.PlaySync();
+                            }
+                            Application.DoEvents();
+                        }
+
+                    }
+
+                }//while (loopFlag2)
+
+
+
+
+                
+            }//while (loopFlag1)
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loopFlag1 = false;
+            loopFlag2 = false;
+            richTextBox1.AppendText("\nEnd Game\n");
+            Application.DoEvents();
+        }
+    }
+}
